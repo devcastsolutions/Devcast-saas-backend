0c28923faa56dab7e0a1f96f9b0fff287d808674
=biruksolomon <=biruksolomonmoges@gmail.com>
2025-12-27 18:21:12 +0300

    adding JWT dependency and add env variable for Jwt in application.properties

--- src/main/java/com/devcast/saas/security/JwtService.java
+++ src/main/java/com/devcast/saas/security/JwtService.java
@@ -1,7 +1,17 @@
 package com.devcast.saas.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${jwt.refresh-expiration}")
+    private long refreshExpiration;
 }
